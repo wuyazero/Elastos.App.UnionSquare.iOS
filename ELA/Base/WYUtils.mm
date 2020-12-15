f7fd506cd4387262fe44343347a50aa0f9003dc6
macdev <macdev@dev.mac>
2020-12-15 12:29:01 +0800

    bugfixes
    fixed log share issues
    fixed DID Card compatibility issues

--- ELA/Base/WYUtils.mm
+++ ELA/Base/WYUtils.mm
@@ -24,6 +24,7 @@
 #import "WYUtils.h"
 #import "MyUtil.h"
 #import "ELWalletManager.h"
+#import <CommonCrypto/CommonDigest.h>
 
 static BOOL useNetworkQueue = NO;
 static NSUncaughtExceptionHandler *nextHandler = nil;
@@ -371,6 +372,19 @@ static NSMutableDictionary *globalDic = nil;
     activityVC.excludedActivityTypes = excludeTypesM;
     
     [vc presentViewController:activityVC animated:YES completion:nil];
+}
+
++ (NSString *)MD5Hash:(NSString *)str {
+    const char * pointer = [str UTF8String];
+    unsigned char md5Buffer[CC_MD5_DIGEST_LENGTH];
+
+    CC_MD5(pointer, (CC_LONG)strlen(pointer), md5Buffer);
+
+    NSMutableString *string = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for (int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
+        [string appendFormat:@"%02x",md5Buffer[i]];
+
+    return string;
 }
 
 @end
